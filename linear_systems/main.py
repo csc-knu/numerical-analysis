91474b13d796c53ac1676c4c310961f395a298f8
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2018-09-10 10:58:06 +0300

    linear systems tests and more

--- linear_systems/main.py
+++ linear_systems/main.py
@@ -7,3 +7,45 @@ Created on Sun Sep  9 16:25:51 2018
 Solving Linear Systems of Algebraic Equations
 """
 
+import numpy as np
+
+
+def gauss(a: np.matrix, b: np.array) -> np.array:
+    """
+    :param a: matrix
+    :param b: vector
+    :return: solution-vector x
+    """
+    eps = 1e-7
+
+    if a.shape[0] != a.shape[1]:
+        raise ValueError(f'Dimensions mismatch: a.rows = {a.shape[0]} != {a.shape[1]} = a.cols')
+
+    if b.shape[0] != a.shape[0]:
+        raise ValueError(f'Dimensions mismatch: a.rows = {a.shape[0]} != {b.shape[0]} = b.rows')
+
+    n = a.shape[0]
+    
+    p = list(range(n))
+
+    for k in range(n):
+        if abs(a[k, k]) < eps:
+            for m in range(k + 1, n):
+                if abs(a[k, m]) >= eps:
+                    a[:, [k, m]] = a[:, [m, k]]
+                    p[k], p[m] = p[m], p[k]
+                    break
+
+        b[k] /= a[k, k]
+        a[k] /= a[k, k]
+
+        for i in range(k + 1, n):
+            b[i] -= a[i, k] * b[k]
+            a[i] -= a[i, k] * a[k]
+
+    x = b
+
+    for i in range(n - 1, -1, -1):
+        x[i] -= sum(a[i, j] * x[j] for j in range(i + 1, n))
+
+    return np.array([x[p[i]] for i in range(n)])
--- linear_systems/tests.py
+++ linear_systems/tests.py
@@ -1,9 +1,83 @@
 import unittest
+import numpy as np
+
+from linear_systems.main import gauss
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+class GaussTestCase(unittest.TestCase):
+    def test_2_by_2(self):
+        a = np.matrix([[1, 2], [3, 4]], dtype=float)
+        b = np.array([1, 1], dtype=float)
+        x = np.array([-1, 1], dtype=float)
+        g = gauss(a, b)
+        for i in range(2):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
+
+        a = np.matrix([[3, -2], [5, 1]], dtype=float)
+        b = np.array([-6, 3], dtype=float)
+        x = np.array([0, 3], dtype=float)
+        g = gauss(a, b)
+        for i in range(2):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
+
+        a = np.matrix([[5, 2], [2, 1]], dtype=float)
+        b = np.array([7, 9], dtype=float)
+        x = np.array([-11, 31], dtype=float)
+        g = gauss(a, b)
+        for i in range(2):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
+
+    def test_3_by_3(self):
+        a = np.matrix([[2, 1, 1], [1, -1, 0], [3, -1, 2]], dtype=float)
+        b = np.array([2, -2, 2], dtype=float)
+        x = np.array([-1, 1, 3], dtype=float)
+        g = gauss(a, b)
+        for i in range(3):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
+
+        a = np.matrix([[1, 2, 3], [3, 5, 7], [1, 3, 4]], dtype=float)
+        b = np.array([3, 0, 1], dtype=float)
+        x = np.array([-4, -13, 11], dtype=float)
+        g = gauss(a, b)
+        for i in range(3):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
+
+        a = np.matrix([[1, 1, 1], [4, 2, 1], [9, 3, 1]], dtype=float)
+        b = np.array([0, 1, 3], dtype=float)
+        x = np.array([.5, -.5, 0], dtype=float)
+        g = gauss(a, b)
+        for i in range(3):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
+
+    def test_4_by_4(self):
+        a = np.matrix([[1, -2, -1, 1], [1, -8, -2, -3], [2, 2, -1, 7], [1, 1, 2, 1]], dtype=float)
+        b = np.array([1, -2, 7, 1], dtype=float)
+        x = np.array([-2, -1, 1, 2], dtype=float)
+        g = gauss(a, b)
+        for i in range(4):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
+
+        a = np.matrix([[3, 3, 6, 3], [3, 1, 5, 1], [2, 1, 4, 2], [1, 3, 3, 2]], dtype=float)
+        b = np.array([6, 2, 1, 6], dtype=float)
+        x = np.array([3, 3, -2, 0], dtype=float)
+        g = gauss(a, b)
+        for i in range(4):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
+
+        a = np.matrix([[2, 2, -3, 3], [3, 2, -1, 1], [1, 1, -2, 2], [2, 4, -3, 2]], dtype=float)
+        b = np.array([-3, -3, -1, -3], dtype=float)
+        x = np.array([2, -5, -11, -10], dtype=float)
+        g = gauss(a, b)
+        for i in range(4):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
+
+    def test_5_by_5(self):
+        a = np.matrix([[1, 1, 0, 0, 0], [1, 1, 1, 0, 0], [0, 1, 1, 1, 0], [0, 0, 1, 1, 1], [0, 0, 0, 1, 1]], dtype=float)
+        b = np.array([1, 4, -3, 2, -1], dtype=float)
+        x = np.array([2, -5, -11, -10], dtype=float)
+        g = gauss(a, b)
+        for i in range(5):
+            self.assertAlmostEqual(g[i], x[i], delta=1e-7)
 
 
 if __name__ == '__main__':
