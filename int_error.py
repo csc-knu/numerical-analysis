5f531ce2036509366981f7b2c972bcfac9a95f61
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2018-11-24 16:29:19 +0200

    int error runge

--- int_error.py
+++ int_error.py
@@ -3,7 +3,7 @@ import unittest
 
 
 def interpolation_error(method: str, M_d_plus_1: float, a: float or None=None, 
-	b: float or None=None,n: int or None=None, d: int or None=None):
+	b: float or None=None, n: int or None=None) -> float:
 	if a is None:
 		a = b - h * n
 	if b is None:
@@ -25,6 +25,22 @@ def interpolation_error(method: str, M_d_plus_1: float, a: float or None=None,
 
 	return c * (b - a) * ((b - a) / n)**(d + 1) * M_d_plus_1
 
+
+def richardson_extrapolation(method: str, sn: float, s2n: float, 
+	d: int or None=None) -> float:
+	if d is None:
+		d = {
+			'left rect': 0,
+			'right rect': 0,
+			'mid rect': 1,
+			'trapezoid': 1,
+			'simpson': 3,
+		}[method]
+
+	rn_main = (s2n - sn) / (2**(d + 1) - 1)
+
+	return s2n + rn_main
+	
 
 if __name__ == '__main__':
     unittest.main()
