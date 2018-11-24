6913f064dc428ed921ffbaffdb63bf344bd361c6
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2018-11-24 18:15:58 +0200

    int gauss

---
+++ integration-1/int_gauss.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import numpy as np
+import unittest
+from typing import Callable
+from math import sqrt
+
+
+def integration_gauss(f: Callable[[float], float], n: int) -> float:
+	""" n in {} 1, 2, 3 """
+	if n == 1:
+		return 2 * f(0)
+	if n == 2:
+		f(-1 / sqrt(3)) + f(1 / sqrt(3))
+	if n == 3:
+		return 5 / 9 * f(-sqrt(3 / 5)) + 8 / 9 * f(0) + 5 / 9 * f(sqrt(3 / 5))
+
+
+def integration_gauss_like(f: Callable[[float], float], n: int, mu: np.array) -> float:
+	""" n = 2 """
+	a1, a2 = (mu[0] * mu[3] - mu[1] * mu[2]) / (mu[1]**2 - mu[0] * mu[2]), \
+		(mu[2]**2 - mu[1] * mu[3]) / (mu[1]**2 - mu[0] * mu[2])
+
+	x1, x2 = (-a1 - sqrt(a1**2 - 4 * a2)) / 2, (-a1 + sqrt(a1**2 - 4 * a2)) / 2
+
+	A1, A2 = (mu[1] - x2 * mu[0]) / (x[1] - x[2]), (mu[1] - x1 * mu[0]) / (x[1] - x[2])
+
+	return A1 * f(x1) + A2 * f(x2)
+
+
+if __name__ == '__main__':
+    unittest.main()
