e0ee94a85036191d742cdca01d8422888f6a73d6
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2018-11-24 19:12:29 +0200

    matrix helder norm

---
+++ matrix_heled_norm.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import numpy as np
+import unittest
+from math import inf, sqrt
+
+
+def matrix_helder_norm(a: np.matrix, p: float) -> float:
+	return pow(np.sum(np.power(np.abs(a), p)), 1 / p) if p != inf else np.max(np.abs(a))
+
+
+class TestMatrixHelderNorm(unittest.TestCase):
+	def test_1(self):
+		a = np.matrix([[1, 2, -3], [4, -5, 6], [7, 8, -9]])
+
+		self.assertEqual(matrix_helder_norm(a, 1), 45)
+
+
+	def test_2(self):
+		a = np.matrix([[1, 2, -3], [4, -5, 6], [7, 8, -9]])
+
+		self.assertAlmostEqual(matrix_helder_norm(a, 2), sqrt(285))
+
+
+	def test_inf(self):
+		a = np.matrix([[1, 2, -3], [4, -5, 6], [7, 8, -9]])
+
+		self.assertAlmostEqual(matrix_helder_norm(a, inf), 9)
+
+
+if __name__ == '__main__':
+	unittest.main()
