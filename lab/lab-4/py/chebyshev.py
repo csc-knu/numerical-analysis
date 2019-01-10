44136bd553e74d956fa36fc258e4928487043c49
Nikita Skybytsyi <n.skybytskyi@gmail.com>
2019-01-10 14:39:02 +0200

    chebyshev Release

--- lab/lab-4/py/chebyshev.py
+++ lab/lab-4/py/chebyshev.py
@@ -1,18 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""chebyshev.py: interpolates function f(x) 
+by a give grid y of values for x in [a, b] 
+via Newton method with Chebyshev nodes."""
 from math import cos, pi
 import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size': 22})
+
 
 
 def interpolate_newton_chebyshev(y, a, b, n):
-	x = [(a + b) / 2 + (b - a) / 2 * cos(pi * (2 * i + 1) / (2 * n)) for i in range(n)]
+	x = [(a + b) / 2 + (b - a) / 2 * cos(pi * (2 * i + 1) / (2 * n)) \
+		for i in range(n)]
 
-	print(f'Вузли інтерполяції:\n\t{[round(xi, _round) for xi in x]}\n')
+	print(f'\nВузли інтерполяції:\n'
+		f'\t{[round(xi, _round) for xi in x]}\n\n')
 
-	print(f'Значення функції у вузлах:\n\t{[round(_, _round) for _ in y]}\n')
+	print(f'Значення функції у вузлах:\n'
+		f'\t{[round(_, _round) for _ in y]}\n\n')
 
 	rr = [x, [(y[i] - y[i + 1]) / (x[i] - x[i + 1]) for i in range(n - 1)]]
 
-	print(f'Розділені різниці 1-го порядку:\n\t{[round(_, _round) for _ in rr[1]]}\n')
+	print(f'Розділені різниці 1-го порядку:\n'
+		f'\t{[round(_, _round) for _ in rr[1]]}\n\n')
 
 	for i in range(2, n):
 		rr.append([0 for _ in range(n - i)])
@@ -20,7 +29,8 @@ def interpolate_newton_chebyshev(y, a, b, n):
 		for j in range(n - i):
 			rr[i][j] = (rr[i - 1][j] - rr[i - 1][j + 1]) / (x[j] - x[j + i])
 
-		print(f'Розділені різниці {i}-го порядку:\n\t{[round(_, _round) for _ in rr[i]]}\n')
+		print(f'Розділені різниці {i}-го порядку:\n'
+			f'\t{[round(_, _round) for _ in rr[i]]}\n\n')
 
 	pn = f'{round(rr[n - 1][0], _round)}'
 
@@ -29,39 +39,56 @@ def interpolate_newton_chebyshev(y, a, b, n):
 
 	pn = f'(x - {round(x[0], _round)}) * ({pn}) + {round(y[0], _round)}'
 
-	print(f'Інтерполюючий багаточлен:\n\tP_{n-1}(x) = {pn}\n')
+	print(f'Інтерполюючий багаточлен:\n'
+		f'\tP_{n-1}(x) = {pn}\n')
 
 	return pn
 
 
-def f(x: float) -> float:
-	return 1 / 2 * (abs(x - 4) + abs(x + 4))
+if __name__ == '__main__':
+	def f(x: float) -> float:
+		return 1 / 2 * (abs(x - 4) + abs(x + 4))
+
+	plt.rcParams.update({'font.size': 22})
+
+	a, b, n, _round, k = -5, 7, 13, 9, 1000
+
+	y = [f((a + b) / 2 + (b - a) / 2 * cos(pi * (2 * i + 1) / (2 * n))) \
+		for i in range(n)]
+
+	pn = interpolate_newton_chebyshev(y, a, b, n)
+
+	_x = [a + i * (b - a) / k for i in range(k + 1)]
+
+	y, _f = [eval(pn) for x in _x], [f(x) for x in _x]
+
+	omega =[y[i] - _f[i] for i in range(k + 1)]
+
+	plt.figure(figsize=(20,20))
+
+	plt.grid(True)
+
+	plt.plot(_x, y, 'b-', label=f'$P_{{{n-1}}}(x)$')
+	plt.plot(_x, _f, 'r--', label=f'$f(x)$')
+	plt.plot(_x, omega, 'g-.', label=f'$\omega_{{{n-1}}}(x)$')
+
+	_x = [(a + b) / 2 + (b - a) / 2 * cos(pi * (2 * i + 1) / (2 * n)) \
+		for i in range(n)]
+
+	y = [f(x) for x in _x]
+
+	plt.scatter(_x, y, c='r', alpha=1)
+
+	plt.legend(loc='upper center', fontsize=22)
+
+	plt.show()
 
 
-a, b, n, _round, k = -5, 7, 13, 9, 1000
-
-y = [f((a + b) / 2 + (b - a) / 2 * cos(pi * (2 * i + 1) / (2 * n))) for i in range(n)]
-
-pn = interpolate_newton_chebyshev(y, a, b, n)
-
-_x = [a + i * (b - a) / k for i in range(k + 1)]
-
-y, _f = [eval(pn) for x in _x], [f(x) for x in _x]
-omega =[y[i] - _f[i] for i in range(k + 1)]
-
-plt.figure(figsize=(20,20))
-
-plt.grid(True)
-
-plt.plot(_x, y, 'b-', label=f'$P_{{{n-1}}}(x)$')
-plt.plot(_x, _f, 'r--', label=f'$f(x)$')
-plt.plot(_x, omega, 'g-.', label=f'$\omega_{{{n-1}}}(x)$')
-
-_x = [(a + b) / 2 + (b - a) / 2 * cos(pi * (2 * i + 1) / (2 * n)) for i in range(n)]
-y = [f(x) for x in _x]
-
-plt.scatter(_x, y, c='r', alpha=1)
-
-plt.legend(loc='upper center', fontsize=22)
-
-plt.show()
+__author__: "Nikita Skybytskyi"
+__copyright__ = "Copyright 2019, KNU"
+__credits__ = ["Nikita Skybytskyi"]
+__license__ = "MIT"
+__version__ = "1.1.1"
+__maintainer__ = "Nikita Skybytskyi"
+__email__ = "n.skybytskyi@knu.ua"
+__status__ = "Production"
